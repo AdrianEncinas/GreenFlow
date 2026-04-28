d0ac331bc5bf141bdc63c225ec715473bd9da6e8
AdrianEncinas <adrianencinas1996@gmail.com>
2026-04-28 17:47:16 +0200

    refactor: remove unused assertion imports in UserControllerTest and UserServiceTest

--- auth-service/src/test/java/com/GreenFlow/v1/controller/UserControllerTest.java
+++ auth-service/src/test/java/com/GreenFlow/v1/controller/UserControllerTest.java
@@ -1,7 +1,6 @@
 package com.GreenFlow.v1.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
--- auth-service/src/test/java/com/GreenFlow/v1/service/UserServiceTest.java
+++ auth-service/src/test/java/com/GreenFlow/v1/service/UserServiceTest.java
@@ -1,7 +1,6 @@
 package com.GreenFlow.v1.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
